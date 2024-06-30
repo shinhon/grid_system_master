2d3767e915bdf0f10528c933896dd8ba73ee8639
Shinhon <shinhonYH@gmail.com>
2024-06-30 11:14:27 +0900

    setup Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1232px',
+    },
+    container: {
+        center: true,
+        padding: {
+            DEFAULT: '1.25rem',
+            md: '2.5rem',
+        },
+    },
+}
